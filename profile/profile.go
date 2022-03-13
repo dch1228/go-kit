eec58432272b51ba92fb646cdf32e4941df2ac0f
杜晨昊 <chenhao.du@qq.com>
2022-03-13 23:34:38 +0800

    profile

---
+++ profile/profile.go
@@ -0,0 +1,32 @@
+package profile
+
+import (
+	"context"
+
+	"github.com/go-kratos/kratos/v2/transport"
+	"github.com/go-kratos/kratos/v2/transport/http"
+	"github.com/go-kratos/kratos/v2/transport/http/pprof"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+type Config struct {
+	Enable bool   `default:"true"`
+	Addr   string `default:":9090"`
+}
+
+type noop struct{}
+
+func (*noop) Start(_ context.Context) error { return nil }
+func (*noop) Stop(_ context.Context) error  { return nil }
+
+func New(cfg Config) transport.Server {
+	if !cfg.Enable {
+		return new(noop)
+	}
+	srv := http.NewServer(
+		http.Address(cfg.Addr),
+	)
+	srv.Handle("/metrics", promhttp.Handler())
+	srv.HandlePrefix("/debug", pprof.NewHandler())
+	return srv
+}
